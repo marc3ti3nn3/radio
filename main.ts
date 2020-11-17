8acea519cd185120d44653c776169e61d58ae252
marc3ti3nn3 <73190943+marc3ti3nn3@users.noreply.github.com>
2020-11-17 14:38:58 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,26 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(2000)
+        basic.clearScreen()
+    } else {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(2000)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(500)
+    basic.clearScreen()
+})
+basic.forever(function () {
+    radio.setGroup(1)
+})
